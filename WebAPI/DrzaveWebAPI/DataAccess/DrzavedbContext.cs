a7db224324edef8a0eba4610f5b53c0b90440dab
robk99 <48883762+robk99@users.noreply.github.com>
2020-01-13 11:11:11 +0100

    Postaviti konekciju na bazu, migracije i updateDB

---
+++ WebAPI/DrzaveWebAPI/DataAccess/DrzavedbContext.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccess
+{
+    public class DrzavedbContext : DbContext
+    {
+        public DrzavedbContext(DbContextOptions<DrzavedbContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Drzava> Drzave { get; set; }
+        public DbSet<Grad> Gradovi { get; set; }
+    }
+}
---
+++ WebAPI/DrzaveWebAPI/DrzaveWebAPI/Migrations/20200113093710_InitialMigration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DrzaveWebAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drzave",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ime = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drzave", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gradovi",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Ime = table.Column<string>(nullable: true),
+                    Populacija = table.Column<int>(nullable: false),
+                    DrzavaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gradovi", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gradovi_Drzave_DrzavaId",
+                        column: x => x.DrzavaId,
+                        principalTable: "Drzave",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gradovi_DrzavaId",
+                table: "Gradovi",
+                column: "DrzavaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gradovi");
+
+            migrationBuilder.DropTable(
+                name: "Drzave");
+        }
+    }
+}
