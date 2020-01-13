c8130dc2275e86f8ac26d506b96da3d38e62a2c1
robk99 <48883762+robk99@users.noreply.github.com>
2020-01-13 14:03:45 +0100

    Izmjeniti SQL file da se slaze sa Code-First pristupom

--- SQL/database_creator.sql
+++ SQL/database_creator.sql
@@ -1,92 +1,87 @@
-\c postgres;
-DROP DATABASE drzaveapp;
-CREATE DATABASE drzaveapp;
+-- \c postgres;
 
-\c drzaveapp;
+-- Resetiranje baze na pocetno stanje
+DELETE FROM "Gradovi";
+DELETE FROM "Drzave";
 
-CREATE TABLE country(id BIGSERIAL NOT NULL PRIMARY KEY, 
-name TEXT NOT NULL);
+ALTER SEQUENCE "Drzave_Id_seq" RESTART WITH 1;
+ALTER SEQUENCE "Gradovi_Id_seq" RESTART WITH 1;
 
-CREATE TABLE city(id BIGSERIAL NOT NULL PRIMARY KEY,
-name TEXT NOT NULL, population INTEGER, 
-countryid INTEGER NOT NULL REFERENCES country(id));
+DROP FUNCTION vrati_gradove_po_drzavi(TEXT);
+DROP FUNCTION vrati_gradove_i_drzave_vece_od(INT);
 
-INSERT INTO country (name) VALUES ('Denmark');
-INSERT INTO country (name) VALUES ('Germany');
-INSERT INTO country (name) VALUES ('Sweeden');
 
-INSERT INTO city(name, population, countryid) VALUES ('Copenhagen', 100250, 1);
-INSERT INTO city(name, population, countryid) VALUES ('Odense', 67327, 1);
+-- Popunjavanje baze
+INSERT INTO public."Drzave" ("Ime") VALUES ('Danska');
+INSERT INTO public."Drzave" ("Ime") VALUES ('Njemacka');
+INSERT INTO public."Drzave" ("Ime") VALUES ('Svedska');
 
-INSERT INTO city(name, population, countryid) VALUES ('Berlin', 88220, 2);
-INSERT INTO city(name, population, countryid) VALUES ('Bonn', 76548, 2);
-INSERT INTO city(name, population, countryid) VALUES ('Aalen', 79324, 2);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Copenhagen', 100250, 1);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Odense', 67327, 1);
 
-INSERT INTO city(name, population, countryid) VALUES ('Stockholm', 109859, 3);
-INSERT INTO city(name, population, countryid) VALUES ('Gothenburg', 99508, 3);
-INSERT INTO city(name, population, countryid) VALUES ('Karlstad', 66912, 3);
-INSERT INTO city(name, population, countryid) VALUES ('Uppsala', 46983, 3);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Berlin', 88220, 2);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Bonn', 76548, 2);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Aalen', 79324, 2);
 
-------- Get_Cities
-CREATE FUNCTION get_cities(_country TEXT) RETURNS TABLE(city_name TEXT, city_population int)
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Stockholm', 109859, 3);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Gothenburg', 99508, 3);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Karlstad', 66912, 3);
+INSERT INTO public."Gradovi"("Ime", "Populacija", "DrzavaId") VALUES ('Uppsala', 46983, 3);
+
+
+--Funkcije:
+-- VRACANJE GRADOVA PO DEFINIRANOJ DRZAVI
+CREATE FUNCTION vrati_gradove_po_drzavi(_drzava TEXT) 
+RETURNS SETOF "Gradovi"
 AS $$
 BEGIN
 
-RETURN query SELECT city.name, city.population FROM city WHERE countryid = 
-(SELECT id from country WHERE country.name = _country);
+RETURN query SELECT * FROM "Gradovi" WHERE "DrzavaId" = 
+(SELECT "Id" from "Drzave" WHERE "Drzave"."Ime" = _drzava);
 IF NOT FOUND THEN
-RETURN query SELECT city.name, city.population FROM city;
+RETURN query SELECT * FROM "Gradovi";
 END IF;
 
 END
 $$
 LANGUAGE plpgsql;
 
-------- Get_Cities_by_Population
-CREATE FUNCTION get_cities_by_population(_population int) 
-RETURNS TABLE (city_name text, city_population int)
+-- VRACANJE GRADOVA I DRZAVA PO DEFINIRANOJ POPULACIJI
+CREATE FUNCTION vrati_gradove_i_drzave_vece_od(_populacija int) 
+RETURNS TABLE (Ime TEXT, Populacija bigint) 
 AS $$
 BEGIN
 
-RETURN query SELECT city.name, city.population FROM city
-WHERE city.population > _population;
-
-END
-$$
-LANGUAGE plpgsql;
-
-------- Get_Countries_by_Population
-CREATE FUNCTION get_countries_by_population(_population int) 
-RETURNS TABLE (country_name text, country_population bigint)
-AS $$
-BEGIN
-
-RETURN query 
+RETURN query (
+SELECT "Gradovi"."Ime", "Gradovi"."Populacija" FROM "Gradovi"
+WHERE "Gradovi"."Populacija" > _populacija
+UNION ALL
 SELECT * FROM
 (
-SELECT country.name,
-SUM(city.population) AS cntry_pop
-FROM city, country
-WHERE city.countryid = country.id
-GROUP BY country.name
-ORDER BY country.name
-
+SELECT "Drzave"."Ime",
+SUM("Gradovi"."Populacija") AS drzave_populacija
+FROM "Gradovi", "Drzave"
+WHERE "Gradovi"."DrzavaId" = "Drzave"."Id"  
+GROUP BY "Drzave"."Ime"
+ORDER BY "Drzave"."Ime"
 ) AS inner_query
-WHERE cntry_pop > _population;
+WHERE drzave_populacija > _populacija );
+
 
 END
 $$
 LANGUAGE plpgsql;
 
-------- Functions_Tests
-\echo '\nThis returns all cities for country: Denmark\n'
-SELECT * FROM get_cities('Denmark');
 
-\echo '\nThis returns all cities by predefined population over 80000\n'
-SELECT * FROM get_cities_by_population(80000);
+-- Testovi funkcija:
+\echo '\nFunkcija vraca sve gradove prema unaprijed definiranoj drzavi: Danska\n'
+SELECT * FROM vrati_gradove_po_drzavi('Danska');
 
-\echo '\nThis returns all countires by predefined population over 182000\n'
-SELECT * FROM get_countries_by_population(182000);
+\echo '\nFunkcija vraca sve gradove i drzave koji imaju populaciju vecu od: 80000\n'
+SELECT * FROM vrati_gradove_i_drzave_vece_od(80000);
+
+
+
 
 
 
