d31a223891d6f2efc912990d07086d76baa3c94c
robk99 <48883762+robk99@users.noreply.github.com>
2020-02-05 18:42:03 +0100

    Prevedi sve unose u SQL fileu na engleski

--- SQL/database_creator.sql
+++ SQL/database_creator.sql
@@ -1,71 +1,72 @@
 -- \c postgres;
 
--- Resetiranje baze na pocetno stanje
-DELETE FROM gradovi;
-DELETE FROM drzave;
+-- Resetting database to default state
+DELETE FROM cities;
+DELETE FROM countries;
+DELETE FROM login_users
 
-ALTER SEQUENCE "Drzave_Id_seq" RESTART WITH 1;
-ALTER SEQUENCE "Gradovi_Id_seq" RESTART WITH 1;
+ALTER SEQUENCE "countries_Id_seq" RESTART WITH 1;
+ALTER SEQUENCE "cities_Id_seq" RESTART WITH 1;
 
-DROP FUNCTION vrati_gradove_po_drzavi(TEXT);
-DROP FUNCTION vrati_gradove_i_drzave_vece_od(INT);
+DROP FUNCTION get_cities_by_country(TEXT);
+DROP FUNCTION vrati_cities_i_countries_vece_od(INT);
 
 
 -- Popunjavanje baze
-INSERT INTO public.drzave (ime) VALUES ('Danska');
-INSERT INTO public.drzave (ime) VALUES ('Njemacka');
-INSERT INTO public.drzave (ime) VALUES ('Svedska');
+INSERT INTO public.countries (name) VALUES ('Denmark');
+INSERT INTO public.countries (name) VALUES ('Germany');
+INSERT INTO public.countries (name) VALUES ('Sweden');
 
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Copenhagen', 100250, 1);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Odense', 67327, 1);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Copenhagen', 100250, 1);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Odense', 67327, 1);
 
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Berlin', 88220, 2);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Bonn', 76548, 2);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Aalen', 79324, 2);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Berlin', 88220, 2);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Bonn', 76548, 2);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Aalen', 79324, 2);
 
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Stockholm', 109859, 3);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Gothenburg', 99508, 3);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Karlstad', 66912, 3);
-INSERT INTO public.gradovi(ime, populacija, drzava_id) VALUES ('Uppsala', 46983, 3);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Stockholm', 109859, 3);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Gothenburg', 99508, 3);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Karlstad', 66912, 3);
+INSERT INTO public.cities(name, population, country_id) VALUES ('Uppsala', 46983, 3);
 
 
---Funkcije:
--- VRACANJE GRADOVA PO DEFINIRANOJ DRZAVI
-CREATE FUNCTION vrati_gradove_po_drzavi(_drzava TEXT) 
-RETURNS SETOF gradovi
+--Functions:
+-- GET CITY BY COUNTRY ID
+CREATE FUNCTION get_cities_by_country(_country TEXT) 
+RETURNS SETOF cities
 AS $$
 BEGIN
 
-RETURN query SELECT * FROM gradovi WHERE drzava_id = 
-(SELECT id from drzave WHERE drzave.ime = _drzava);
+RETURN query SELECT * FROM cities WHERE country_id = 
+(SELECT id from countries WHERE countries.name = _country);
 IF NOT FOUND THEN
-RETURN query SELECT * FROM gradovi;
+RETURN query SELECT * FROM cities;
 END IF;
 
 END
 $$
 LANGUAGE plpgsql;
 
--- VRACANJE GRADOVA I DRZAVA PO DEFINIRANOJ POPULACIJI
-CREATE FUNCTION vrati_gradove_i_drzave_vece_od(_populacija int) 
-RETURNS TABLE (Ime TEXT, Populacija bigint) 
+-- GET CITIES AND COUNTRIES BY PREDEFINED POPULATION
+CREATE FUNCTION get_cities_and_countries_with_greater_population_then(_population int) 
+RETURNS TABLE (name TEXT, population bigint) 
 AS $$
 BEGIN
 
 RETURN query (
-SELECT gradovi.ime, gradovi.populacija FROM gradovi
-WHERE gradovi.populacija > _populacija
+SELECT cities.name, cities.population FROM cities
+WHERE cities.population > _population
 UNION ALL
 SELECT * FROM
 (
-SELECT drzave.ime,
-SUM(gradovi.populacija) AS drzave_populacija
-FROM gradovi, drzave
-WHERE gradovi.drzava_id = drzave.id  
-GROUP BY drzave.ime
-ORDER BY drzave.ime
+SELECT countries.name,
+SUM(cities.population) AS countries_population
+FROM cities, countries
+WHERE cities.country_id = countries.id  
+GROUP BY countries.name
+ORDER BY countries.name
 ) AS inner_query
-WHERE drzave_populacija > _populacija );
+WHERE countries_population > _population );
 
 
 END
@@ -73,12 +74,12 @@ $$
 LANGUAGE plpgsql;
 
 
--- Testovi funkcija:
-\echo '\nFunkcija vraca sve gradove prema unaprijed definiranoj drzavi: Danska\n'
-SELECT * FROM vrati_gradove_po_drzavi('Danska');
+--Test functions:
+\echo '\nFunction returns all cities by predefined country Id: Denmark\n'
+SELECT * FROM get_cities_by_country('Denmark');
 
-\echo '\nFunkcija vraca sve gradove i drzave koji imaju populaciju vecu od: 80000\n'
-SELECT * FROM vrati_gradove_i_drzave_vece_od(80000);
+\echo '\nFunction returns all cities and countries that have population over: 80000\n'
+SELECT * FROM get_cities_and_countries_with_greater_population_then(80000);
 
 
 
