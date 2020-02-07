0cf652a70ff4b76925980869c7ac3edb766c8590
robk99 <48883762+robk99@users.noreply.github.com>
2020-02-07 17:53:48 +0100

    Postaviti toastr prilikom greske komunikacije sa serverom

--- Angular/src/app/drzave-app/shared/services/http/http-error-interceptor.service.ts
+++ Angular/src/app/drzave-app/shared/services/http/http-error-interceptor.service.ts
@@ -3,13 +3,13 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { LoginService } from 'src/app/login/login-service/login.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class HttpErrorInterceptorService implements HttpInterceptor {
 
 
-  constructor(private loginService: LoginService ){}
+  constructor(private loginService: LoginService, private toastr: ToastrService ){}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
@@ -20,9 +20,11 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
           
           if (error.status == 0) {
             errorMessage = `Error in communication with server: ${error.message}`;
+            this.toastr.error('Error in communication with server!');
+            this.loginService.logOut();
           } 
           else if (error.status == 400) {
-            return throwError(`Invalid username or password - ${error.message}`);
+            errorMessage = `Invalid username or password - ${error.message}`;
           }
           else if (token && this.loginService.isTokenExpired()) {
             errorMessage = `TOKEN EXPIRED - ${error.message}`;
