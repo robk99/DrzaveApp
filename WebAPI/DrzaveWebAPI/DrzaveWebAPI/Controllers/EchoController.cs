cb32fa5dbcc18f2fc1fec58a42e4b0f7199af209
robk99 <robert.kamenski.rld@gmail.com>
2020-07-28 07:56:28 +0200

    Make EchoController for endpoint testing

---
+++ WebAPI/DrzaveWebAPI/DrzaveWebAPI/Controllers/EchoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DrzaveWebAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class EchoController : Controller
+    {
+        // GET: api/echo/someText
+        [HttpGet]
+        [Route("api/echo/{attributeString}")]
+        public IEnumerable<string> Echo([FromRoute] string attributeString)
+        {
+            return new string[] { $"{attributeString} to you too Guest!" };
+        }
+
+
+        // GET: api/authecho/someText
+        [HttpGet, Authorize]
+        [Route("api/authecho/{attributeString}")]
+        public IEnumerable<string> AuthEcho(string attributeString)
+        {
+            return new string[] { $"{attributeString} to you too Authorized user!" };
+        }
+    }
+}
